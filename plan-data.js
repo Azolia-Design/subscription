645c5a1f8dca983b0ca80e93228fe8cb1c1babbe
nqnduy <nqn.duy@gmail.com>
2024-02-05 12:36:20 +0700

    update performance

---
+++ plan-data.js
@@ -0,0 +1,9 @@
+export const planListing = new Map([
+    [0, {priceId: 'price_1OfJjyAyaaWxy2CsH7LieoXV', name: 'Trial', method: 'payment'}],
+    [1, {priceId: 'price_1OfJjyAyaaWxy2CsH7LieoXV', name: 'Standard Monthly', method: 'subscription'}],
+    [2, {priceId: 'price_1OfJjyAyaaWxy2CsH7LieoXV', name: 'Standard Quarterly', method: 'subscription'}],
+    [3, {priceId: 'price_1OfJjyAyaaWxy2CsH7LieoXV', name: 'Standard Yearly', method: 'subscription'}],
+    [4, {priceId: 'price_1OfJjyAyaaWxy2CsH7LieoXV', name: 'Pro Monthly', method: 'subscription'}],
+    [5, {priceId: 'price_1OfJjyAyaaWxy2CsH7LieoXV', name: 'Pro Quarterly', method: 'subscription'}],
+    [6, {priceId: 'price_1OfJjyAyaaWxy2CsH7LieoXV', name: 'Pro Yearly', method: 'subscription'}]
+])
