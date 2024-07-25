f89c5330b7aabf42d0b20ca9cb15db6e45c0f6c7
khanh0601 <89721214+khanh0601@users.noreply.github.com>
2024-07-25 09:35:20 +0700

    update popup

--- src/js/pages/home/index.js
+++ src/js/pages/home/index.js
@@ -1210,7 +1210,49 @@ const home = {
             initAccordion('.home-faq-content-listing');
         }
         homeFaq();
-
+        
+        // popup
+        const initPopup = (name) => {
+            let popupWrap = $(`[data-popup-${name}='wrap']`);
+            const popupAction = {
+                open: () => {
+                    popupWrap.addClass('active');
+                    requestAnimationFrame(() => $('.header-bar').addClass('force'));
+                    lenis.stop();
+                },
+                close: () => {
+                    if (!popupWrap.hasClass('active')) return;
+                    setTimeout(() => {
+                        popupWrap.removeClass('active');
+                        $('.header-bar').removeClass('force');
+                    }, 100)
+                    lenis.start();
+                }
+            }
+            $(`[data-popup-${name}]`).on('click', function (e) {
+                if ($(this).attr(`data-popup-${name}`) === 'open') {
+                    e.preventDefault();
+                    popupAction.open();
+                }
+                else if ($(this).attr(`data-popup-${name}`) === 'close') {
+                    e.preventDefault();
+                    popupAction.close();
+                }
+                else return;
+            });
+    
+            $(window).on('click', (e) => {
+                if (!$(`[data-popup-${name}='open']:hover`).length) {
+                    if ($(`[data-popup-${name}="wrap"]`).hasClass('active')) return;
+                        popupAction.close();
+                }
+            })
+    
+            $(`[data-popup-${name}="wrap"] .popup-overlay`).on('click', function (e) {
+                popupAction.close();
+            })
+        }
+        initPopup('contact')
     },
     beforeLeave() {
         console.log(`leave ${this.namespace}`);
